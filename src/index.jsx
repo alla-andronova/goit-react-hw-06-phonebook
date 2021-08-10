701e2fbb840a267515b416317a5be0859e9561d8
alla-andronova <allaitcreative@gmail.com>
2021-08-10 18:52:06 +0300

    add localstorage

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
@@ -7,8 +8,10 @@ import store from './redux/store/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,20 @@
 // import { createStore } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contacts from '../slices/contacts';
 import filter from '../slices/filter';
 // import { composeWithDevTools } from 'redux-devtools-extension';
@@ -7,14 +22,29 @@ import filter from '../slices/filter';
 // import { rootReducer } from '../reducers/index';
 
 // const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: 'hello',
+  storage,
+};
 
 const rootReducer = {
   contacts,
   filter,
 };
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers(rootReducer),
+);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
